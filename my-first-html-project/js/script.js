f24cc94f4c32fcef66b055d36a65fdc61c6ea4b7
cod-arsh <iamarshfaisal@gmail.com>
2025-11-06 12:10:36 -0600

    error

--- my-first-html-project/js/script.js
+++ my-first-html-project/js/script.js
@@ -17,7 +17,3 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 300);
     });
 });
-
-
-$ git config --global user.name "John Doe"
-$ git config --global user.email johndoe@example.com
